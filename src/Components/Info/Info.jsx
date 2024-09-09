5e79b5f06c4f4175315498165b8c71bb76a91902
karapetyanhakob4 <karapetyanhakob4@gmail.com>
2024-09-09 23:30:10 +0400

    Last finish

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -10,7 +10,9 @@ export default function Info(params) {
           <div className="FirstStop">
             <h2>Օրվա ծրագիր</h2>
             <span className="time2">15:00</span>
-            <p className="name">Սուրբ Հովհաննես եկեղեցի</p>
+            <p className="name">
+              Սուրբ Հովհաննես <br /> եկեղեցի
+            </p>
             <p className="location">Հասցե՝ Արտաշատ, Երևանյան 2</p>
             <img src={charch} alt="" className="locImg" />
             <a
@@ -23,7 +25,9 @@ export default function Info(params) {
           </div>
           <div className="SecondStop">
             <span className="time2">17:30</span>
-            <p className="name">«Vahagn Hall» հանդիսությունների սրահ</p>
+            <p className="name">
+              «Vahagn Hall» <br /> հանդիսությունների <br /> սրահ
+            </p>
             <p className="location">Հասցե՝ Այգավան, Թումանյան 29 </p>
             <img src={hall} alt="" className="locImg" />
             <a
